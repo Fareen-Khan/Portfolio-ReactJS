7773a61097fe356b9d625f996387b4ce0a654796
Fareen Khan <kfareen34@gmail.com>
2025-05-02 12:55:11 -0400

    got all work formatted

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,31 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs, orderBy, query, Timestamp } from "firebase/firestore"
 import { db } from "../lib/firebase";
 
 type Job = {
-  id: string;           
+  id: string;
   company: string;
   role: string;
-  start_date: string;    // store your dates as ISO strings in Firestore
-  end_date?: string;
+  start_date: Date;    // store your dates as ISO strings in Firestore
+  end_date?: Date;
   description: string;
 };
 
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 export default function Work() {
   const [jobs, setJobs] = useState<(Job & { id: string })[]>([]);
 
   useEffect(() => {
-    const fetchProjects = async () => {
-      const querySnapshot = await getDocs(collection(db, "work"));
-      const work = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data() as Omit<Job, "id">,
-      }));
-      console.log("Loaded projects:", work);
+    const fetchWork = async () => {
+      const q = query(collection(db, "work"), orderBy("start_date", "desc"));
+      const snapshot = await getDocs(q);
+
+      const work: Job[] = snapshot.docs.map((doc) => {
+        const data = doc.data() as {
+          company: string;
+          role: string;
+          start_date: Timestamp;
+          end_date?: Timestamp;
+          description: string;
+        };
+
+        return {
+          id: doc.id,
+          company: data.company,
+          role: data.role,
+          start_date: data.start_date.toDate(),
+          end_date: data.end_date ? data.end_date.toDate() : undefined,
+          description: data.description,
+        };
+      });
+
+      console.log("Loaded work:", work);
       setJobs(work);
     };
-    fetchProjects();
 
+    fetchWork();
   }, []);
   return (
     <section id="work" className="mb-16">
@@ -34,7 +52,10 @@ export default function Work() {
         {jobs.map((job) => (
           <li key={job.id}>
             <h3 className="text-xl font-medium text-white">
-              {job.company} • {job.start_date} — {job.end_date || "Present"}
+              {job.company} •{" "}
+              {/* Format your Date any way you like */}
+              {months[job.start_date.getMonth()]} {job.start_date.getFullYear()} –{" "}
+              {job.end_date ? months[job.end_date.getMonth()] + " " + job.end_date.getFullYear() : "Present"}
             </h3>
             <p className="text-gray-400">{job.description}</p>
           </li>
